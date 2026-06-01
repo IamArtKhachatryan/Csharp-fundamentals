db14207daa4d2fcba5b385aca3b17a902302cd8c
Artur <artur.xach.art@gmail.com>
2026-06-01 23:36:57 +0400

    First commit

---
+++ Homework_3/Task_1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task_1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string? input = Console.ReadLine();
+            Console.WriteLine(
+                checker(input)
+                    ? "The string is correct"
+                    : "The string is incorrect"
+            );
+        }
+
+        private static bool checker(string? input)
+        {
+            if (string.IsNullOrEmpty(input)) return false;
+            MyStack stack = new MyStack();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(' || input[i] == '{' || input[i] == '[') {
+                    stack.Push(input[i]); continue;
+                }
+                if (stack.IsEmpty()) return false;
+                switch (stack.Peek(), input[i])
+                {
+                    case ('(', ')'):
+                    case ('{', '}'):
+                    case ('[', ']'):
+                        stack.Pop();
+                        break;
+                    default: return false;
+                }
+            }
+            return stack.IsEmpty();
+        }
+    }
+}
---
+++ Homework_3/Task_2/MyStack.cs
@@ -0,0 +1,45 @@
+﻿namespace Task_2
+{
+    internal class MyStack
+    {
+        private (int,int)[] _coordinats;
+        private int _count;
+
+        public MyStack(int capacity = 0)
+        {
+            if (capacity < 0) throw new ArgumentException("Capacity cannot be negative");
+            _coordinats = new (int,int)[capacity];
+            _count = 0;
+        }
+
+        public void Push((int x, int y) coord)
+        {
+            if (_count >= _coordinats.Length) Resize();
+            _coordinats[_count++] = coord;
+        }
+
+        private void Resize()
+        {
+            (int,int)[] _temp = new (int,int)[(_coordinats.Length == 0) ? 4 : 2 * _coordinats.Length];
+            for (int i = 0; i < _coordinats.Length; i++)
+                _temp[i] = _coordinats[i];
+            _coordinats = _temp;        
+        }
+        public void Pop()
+        {
+            if (IsEmpty()) throw new InvalidOperationException("Cannot pop: stack is empty");
+                _count--;
+        }
+
+        public (int,int) Peek()
+        {
+            if (IsEmpty()) throw new InvalidOperationException("Stack is empty");
+            return _coordinats[_count - 1];
+        }
+
+        public bool IsEmpty()
+        {
+            return _count == 0;
+        }
+    }
+}
